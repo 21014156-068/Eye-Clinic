b38130f9a090f8a436a3e8617413c0d938ca0080
21014156-068 <businessceo79@gmail.com>
2026-05-04 19:22:22 +0500

    Fix CORS header by stripping trailing slash from CLIENT_URL

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -2,7 +2,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const requiredVars = ["MONGODB_URI", "MONGODB_DB_NAME", "JWT_SECRET", "ADMIN_EMAIL", "ADMIN_PASSWORD"];
+const requiredVars = [
+  "MONGODB_URI",
+  "MONGODB_DB_NAME",
+  "JWT_SECRET",
+  "ADMIN_EMAIL",
+  "ADMIN_PASSWORD",
+];
 
 for (const key of requiredVars) {
   if (!process.env[key]) {
@@ -14,10 +20,12 @@ export const env = {
   adminEmail: process.env.ADMIN_EMAIL,
   adminName: process.env.ADMIN_NAME || "EyeCon Admin",
   adminPassword: process.env.ADMIN_PASSWORD,
-  clientUrl: process.env.CLIENT_URL || "http://localhost:5173",
+  clientUrl: (process.env.CLIENT_URL || "http://localhost:5173").replace(
+    /\/$/,
+    "",
+  ),
   jwtSecret: process.env.JWT_SECRET,
   mongoDbName: process.env.MONGODB_DB_NAME,
   mongoUri: process.env.MONGODB_URI,
   port: Number(process.env.PORT || 5000),
 };
-
